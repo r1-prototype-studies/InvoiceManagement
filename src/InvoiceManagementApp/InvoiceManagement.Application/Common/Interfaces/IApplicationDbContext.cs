41c8f63e00da2fb42e896d865250e4665948da4a
laptop1 <laptop1@gmail.com>
2020-02-16 00:47:02 -0500

    Add Interfaces

---
+++ src/InvoiceManagementApp/InvoiceManagement.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using InvoiceManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InvoiceManagement.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Invoice> Invoices { get; set; }
+        DbSet<InvoiceItem> InvoiceItems { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
