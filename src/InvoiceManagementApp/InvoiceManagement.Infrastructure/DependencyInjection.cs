d1d287143b21566625f8003213852adf7444fb00
laptop1 <laptop1@gmail.com>
2020-02-15 23:15:41 -0500

    Configure Identity server

---
+++ src/InvoiceManagementApp/InvoiceManagement.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,33 @@
+﻿using InvoiceManagement.Infrastructure.Data;
+using InvoiceManagement.Infrastructure.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InvoiceManagement.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddDefaultIdentity<ApplicationUser>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddIdentityServer()
+                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+            services.AddAuthentication()
+                .AddIdentityServerJwt();
+
+            return services;
+        }
+    }
+}
