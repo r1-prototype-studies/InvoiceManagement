cad5493a45878f873a37d89001fe3ea160d75f58
laptop1 <laptop1@gmail.com>
2020-02-16 11:55:55 -0500

    Create Invoices API

---
+++ src/InvoiceManagementApp/InvoiceManagementApp.Api/Controllers/InvoicesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InvoiceManagement.Application.Invoices.Commands;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InvoiceManagementApp.Api.Controllers
+{
+    [Authorize]
+    public class InvoicesController : ApiController
+    {
+        [HttpPost]
+        public async Task<ActionResult<int>> Create(CreateInvoiceCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+    }
+}
